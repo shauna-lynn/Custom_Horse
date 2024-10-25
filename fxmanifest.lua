6c9ce95d2633c9b1c97e8560cf5aac5ecfcba298
Shauna | Elyxr <59288101+shauna-lynn@users.noreply.github.com>
2024-10-24 21:42:18 -0400

    Initial Upload.
    From stream - working vers.
    Horse height 1.1

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+ 
+files {
+    'metapeds.ymt'
+}
+
+data_file 'PED_METADATA_FILE' 'metapeds.ymt'
+
